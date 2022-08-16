63884270ca9156de5787a276d2e8277bc3a6f6df
Rdm-jony <jonydascse@gmail.com>
2022-08-17 03:14:59 +0600

    update erreor

--- bap-er-bank/dom.js
+++ bap-er-bank/dom.js
@@ -1,3 +1,4 @@
+
 document.getElementById("login-btn").addEventListener("click", function () {
     const TargetEmail = document.getElementById("email");
     const emailValue = TargetEmail.value;
@@ -5,17 +6,41 @@ document.getElementById("login-btn").addEventListener("click", function () {
     const targetPassword = document.getElementById("password");
     const passwordValue = targetPassword.value;
 
+    const targetLogIn = document.getElementById("error-msg");
+
     if (emailValue == "") {
-        return alert("you dont fill up the email field!");
+        const p = document.createElement("p");
+        p.innerText = "You dont fill up the email field!";
+        p.style.color = "red";
+        p.style.fontSize = ".8rem"
+        p.style.marginBottom = "5px"
+        targetLogIn.appendChild(p);
+        p.previousSibling.remove();
+        return;
     };
+
     if (passwordValue == "") {
-        return alert("you dont fill up the password field!");
+        const p = document.createElement("p");
+        p.innerText = "You dont fill up the password field!";
+        p.style.color = "red";
+        p.style.fontSize = ".8rem"
+        p.style.marginBottom = "5px"
+        targetLogIn.appendChild(p);
+        p.previousSibling.remove();
+        return;
     }
     if (emailValue == "jonydascse@gmail.com" && passwordValue == "jonydas") {
         window.location.href = "bank.html";
         return;
     }
     else {
-        return alert("incorrect!!");
+        const p = document.createElement("p");
+        p.innerText = "Incorrect Pasword or email!";
+        p.style.color = "red";
+        p.style.fontSize = ".8rem"
+        p.style.marginBottom = "5px"
+        targetLogIn.appendChild(p);
+        p.previousSibling.remove();
+        return;
     }
 })
